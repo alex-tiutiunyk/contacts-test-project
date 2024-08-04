a44c4fcb69b220e3f73e05fe8925678cabafbf7d
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-08-04 18:21:56 +0300

    added MUI and some initial layout

--- src/routes/Contacts.jsx
+++ src/routes/Contacts.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
+import CreateContact from '../components/CreateContact';
+import { NavLink } from 'react-router-dom';
+
 
 const Contacts = () => {
   return (
-    <>
-      <h1>Contacts Page</h1>
-      <ul>
-        <li><NavLink to={'1'}>Contact 1</NavLink></li>
-        <li><NavLink to={'2'}>Contact 2</NavLink></li>
-      </ul>
-    </>
+    <Container maxWidth="lg">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8} sx={{order: 1}}>
+            <h1>Contacts</h1>
+            <ul>
+              <li><NavLink to={'1'}>Contact 1</NavLink></li>
+              <li><NavLink to={'2'}>Contact 2</NavLink></li>
+            </ul>
+          </Grid>
+          <Grid item xs={4} sx={{order: 0}}>
+            <h2>Create Contact</h2>
+            <CreateContact />
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   )
 }
 
