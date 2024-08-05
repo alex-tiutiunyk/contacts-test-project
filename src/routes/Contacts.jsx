05f15bcd934112c4a7622f7fccd3f4a930a6f1a3
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-08-05 12:08:03 +0300

    added simple code to get and view contacts

--- src/routes/Contacts.jsx
+++ src/routes/Contacts.jsx
@@ -1,25 +1,80 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import { Container } from '@mui/material';
+import {Avatar, Card, Chip, Container, List, ListItem, Typography} from '@mui/material';
+import { ListItemContent, ListItemDecorator } from '@mui/joy';
 import CreateContact from '../components/CreateContact';
 import { NavLink } from 'react-router-dom';
 
 
 const Contacts = () => {
+  const token = 'VlP9cwH6cc7Kg2LsNPXpAvF6QNmgZn';
+  const sort = 'created:desc';
+  const url = `https://cors-anywhere.herokuapp.com/https://live.devnimble.com/api/v1/contacts?access_token=${token}&sort=${sort}`;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Something is broken!')
+        const contacts = await response.json();
+        console.log(contacts.resources)
+        setData(contacts.resources);
+        // console.log(contacts.resources[28].fields.email[0].value)
+        console.log(contacts.resources[28].fields['first name'][0].value)
+        // console.log(contacts.resources[28])
+      } catch(err) {
+        console.log(err)
+      }
+
+    }
+    getData()
+  },[])
+
+
   return (
     <Container maxWidth="lg">
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           <Grid item xs={8} sx={{order: 1}}>
-            <h1>Contacts</h1>
-            <ul>
-              <li><NavLink to={'1'}>Contact 1</NavLink></li>
-              <li><NavLink to={'2'}>Contact 2</NavLink></li>
-            </ul>
+            <Typography level="h1">Contacts</Typography>
+            <List
+              aria-labelledby="ellipsis-list-demo"
+              sx={{ '--ListItemDecorator-size': '56px' }}
+            >
+              {data.map(item => (
+                <ListItem key={item.id}>
+                  <ListItemDecorator>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={item.avatar_url}
+                    />
+                  </ListItemDecorator>
+                  <ListItemContent>
+                    <Typography level="title-lg">
+                      {
+                        (item.fields['first name']) ? (item.fields['first name'][0].value + ' ') : ''
+                        (item.fields['last name']) ? (item.fields['last name'][0].value) : ''
+                      }
+                    </Typography>
+                    <Typography level="body-md ">
+                      Email: {(item.fields.email) ? (item.fields.email[0].value) : 'no email'}
+                    </Typography>
+                    <Typography level="body-md ">
+                      Date: {item.created}
+                    </Typography>
+                    <ul>
+                      <li>Tag1</li>
+                      <li>Tag2</li>
+                    </ul>
+                  </ListItemContent>
+                </ListItem>
+              ))}
+            </List>
           </Grid>
           <Grid item xs={4} sx={{order: 0}}>
-            <h2>Create Contact</h2>
+            <Typography level="h1">Create Contact</Typography>
             <CreateContact />
           </Grid>
         </Grid>
